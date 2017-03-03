46e336ffb8f78135caf4886dc7c2fa4a157b2954
nefaerious <samshin102@gmail.com>
2017-03-03 10:56:13 -0600

    added sass to project

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var sassMiddleware = require('node-sass-middleware');
 
 var port = process.env.PORT || 3000;
+
+app.use(sassMiddleware({
+   /* Options */
+   src: path.join(__dirname, 'sass'),
+   dest: path.join(__dirname, 'stylesheets'),
+   debug: true,
+   outputStyle: 'compressed',
+   prefix:  '/stylesheets'
+}));
 
 app.set('port', port);
 app.use(express.static('public'));
