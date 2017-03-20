d4a8b0b972e367025b1488e363c8a799c29ba789
nefaerious <samshin102@gmail.com>
2017-03-20 09:07:41 -0500

    fixes to routing for blog

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/Blogs/', (req, res) => {
-  res.render('index');
+router.get('/', (req, res) => {
+  res.render('blog/index');
 });
 
 module.exports = router;
