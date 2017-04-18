91b40a28d1d5f6ce945d3f0cb82177e8edfe2dcd
nefaerious <samshin102@gmail.com>
2017-04-18 12:19:45 -0500

    testing again for blog page

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('/blogs/index');
+  res.render('/blogs');
 });
 
 module.exports = router;
