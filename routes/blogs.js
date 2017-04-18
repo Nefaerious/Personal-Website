59117e6ae0cd8a8ea7eed5fb94ead0822ba07e51
nefaerious <samshin102@gmail.com>
2017-04-18 12:37:59 -0500

    try

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('/blogs/');
+  res.redirect('/blogs/');
 });
 
 module.exports = router;
