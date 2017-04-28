748a7a8d39b97a87853347f39ca512042a4f93af
nefaerious <samshin102@gmail.com>
2017-04-28 09:33:54 -0500

    reversed previous commit changes and updated express version

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.use('/', require('./routes/index'));
-app.use('/blogs', path.join(__dirname, require('routes/blogs')));
+app.use('/blogs', require('./routes/blogs'));
 app.use('/stylesheets', express.static(path.join(__dirname, 'stylesheets')));
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
