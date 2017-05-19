1b74986c2104720011a548b5592d1b1c0ca57060
nefaerious <samshin102@gmail.com>
2017-05-19 10:05:14 -0500

    testing react

--- views/etc/game.js
+++ views/etc/game.js
@@ -1,9 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
+class MyComponent extends React.Component {
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      "Hello "
+    );
+  }
+}
+
+ReactDOM.render(MyComponent, document.getElementById('root'));
 
 alert('hello');
